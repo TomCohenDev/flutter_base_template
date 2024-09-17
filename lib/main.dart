ecfe9bcbb0701f145235133bdb1dbe524059a101
Tom Cohen <tom.cohen99@gmail.com>
2024-09-17 14:38:56 +0300

    add basic templating

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const PortfolioApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class PortfolioApp extends StatelessWidget {
+  const PortfolioApp({super.key});
 
   @override
   Widget build(BuildContext context) {
